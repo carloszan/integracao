2bbf29d988854245ceae01cf300c251e3d9f6fab
Carlos <undefined751@gmail.com>
2016-06-25 14:03:09 -0300

    agendado com sucesso

--- app/controllers/student_books_controller.rb
+++ app/controllers/student_books_controller.rb
@@ -1,5 +1,32 @@
 class StudentBooksController < ApplicationController
+  before_action :already_booked, only: [:edit, :update]
   def index
-    @books = Book.all
+    @books = Book.not_booked
+  end
+
+  def show
+    @book = Book.find_by(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.student = current_student
+
+    if @book.update_attributes(books_params)
+      flash[:notice] = "Agendado com sucesso"
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def books_params
+    params.require(:book).permit(:hour)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,12 @@ class Book < ActiveRecord::Base
   validates :hour, :employee, presence: true
   belongs_to :employee
   belongs_to :student
+
+  def formated_hour
+    self.hour.to_formatted_s(:short)
+  end
+
+  def self.not_booked
+    self.where.not(student_id: nil)
+  end
 end
