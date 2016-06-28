2ade5ca5d73e11b15e103d939c66dae4a2cf4704
Carlos <undefined751@gmail.com>
2016-06-28 20:25:26 -0300

    adicionado teste para verificar que não pode registrar tempos invalidos

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :book do
     hour "2016-06-21 13:23:53"
-    employee ""
-    student ""
+    employee { FactoryGirl.create(:employee) }
+    student nil
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  
+
   describe "presence" do
     it { validate_presence_of :hour }
     it { validate_presence_of :employee }
+  end
+
+  describe "hour" do
+    it "cant be past time" do
+      b = build(:book)
+
+      b.hour = Time.now - 10
+
+      expect(b).not_to be_valid
+
+      b.hour = Time.now + 10
+
+      expect(b).to be_valid
+    end
   end
 end
